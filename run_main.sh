7e9e91283c8386235776d6d7d49741cfdf8eddf1
anonymous <yanghang07@kuaishou.com>
2025-08-22 12:41:55 +0800

    fix

--- run_main.sh
+++ run_main.sh
@@ -2,8 +2,4 @@ python ./dataset/deal_inter.py
 python ./dataset/deal_item.py
 python ./dataset/deal_data.py
 python -u main.py train_config/sft_douban.yaml
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream-b/master
 python -u eval/gen_eval.py
